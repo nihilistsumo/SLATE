3b8b5cec64e72a8cb7e2c03302bac375af06f518
Sumanta  Kashyapi <sk1105@wildcats.unh.edu>
2020-03-31 11:17:56 -0400

    change temp emb

--- src/data/temp_emb_dict.py
+++ src/data/temp_emb_dict.py
@@ -3,7 +3,7 @@ import numpy as np
 import json
 import argparse
 
-def create_temp_emb_dir(emb_dir, emb_file_prefix, emb_paraids_file, query_attn_data_file, outfile, batch_size=10000):
+def create_temp_emb_dir(emb_dir, emb_file_prefix, emb_paraids_file, query_attn_data_file, outdir, outfile, batch_size=10000):
     all_ids = np.load(emb_paraids_file)
     all_id_part_dict = {}
     for i in range(all_ids.size):
@@ -11,7 +11,6 @@ def create_temp_emb_dir(emb_dir, emb_file_prefix, emb_paraids_file, query_attn_d
     part_para_dict = {}
 
     p_list = set()
-    pemb_dict = {}
     with open(query_attn_data_file, 'r') as qd:
         for l in qd:
             p_list.add(l.split('\t')[2])
@@ -26,14 +25,18 @@ def create_temp_emb_dir(emb_dir, emb_file_prefix, emb_paraids_file, query_attn_d
     print('part para dict created')
     print(str(len(part_para_dict))+' individual emb files to be used')
     i = 0
+    paras = []
+    embs = []
     for pt in part_para_dict.keys():
         emb_vecs = np.load(emb_dir + '/' + emb_file_prefix + '-part' + str(pt) + '.npy')
         for para in part_para_dict[pt]:
-            pemb_dict[para] = emb_vecs[all_id_part_dict[para][1]]
+            paras.append(para)
+            embs.append(emb_vecs[all_id_part_dict[para][1]])
         i += 1
         print(i)
 
-    np.save(outfile, pemb_dict)
+    np.save(outdir+'/'+outfile+'.npy', np.array(embs))
+    np.save(outdir+'/paraids_'+outfile+'.npy', np.array(paras))
 
 def main():
     parser = argparse.ArgumentParser(description='Create temp para emb dict')
@@ -42,15 +45,17 @@ def main():
     parser.add_argument('-pre', '--emb_prefix', help='Embedding file prefix')
     parser.add_argument('-qp', '--qry_attn_file', help='Path to query attn file')
     parser.add_argument('-b', '--batch', help='Batch size for each embedding shards')
-    parser.add_argument('-out', '--outfile', help='Path to output file')
+    parser.add_argument('-od', '--outdir', help='Path to output dir')
+    parser.add_argument('-of', '--outfile', help='output temp emb file name without .npy')
     args = vars(parser.parse_args())
     emb_dir = args['emb_dir']
     emb_paraids_file = args['emb_paras']
     prefix = args['emb_prefix']
     qa_file = args['qry_attn_file']
     batch = int(args['batch'])
+    outdir = args['outdir']
     outfile = args['outfile']
-    create_temp_emb_dir(emb_dir, prefix, emb_paraids_file, qa_file, outfile, batch)
+    create_temp_emb_dir(emb_dir, prefix, emb_paraids_file, qa_file, outdir, outfile, batch)
 
 if __name__ == '__main__':
     main()
