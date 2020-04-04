908e8ab93935043271609f69524e13c90b420964
Sumanta  Kashyapi <sk1105@wildcats.unh.edu>
2020-04-03 22:07:45 -0400

    on the fly embeddings

--- src/keras/para_train.py
+++ src/keras/para_train.py
@@ -21,23 +21,131 @@ import itertools
 TRAIN_TSV = '/home/sumanta/Documents/SiameseLSTM_data/by1train-discrim-bal.tsv'
 TEST_TSV = '/home/sumanta/Documents/SiameseLSTM_data/by1test.tsv'
 TRAIN_EMB_PIDS = '/media/sumanta/Seagate Backup Plus Drive/SentenceBERT_embeddings/sentbert_embeddings_by1train/bert-base-passage-wiki-sec-mean-sentwise/paraids_sents.npy'
-TRAIN_EMB_VECS = '/media/sumanta/Seagate Backup Plus Drive/SentenceBERT_embeddings/sentbert_embeddings_by1train/bert-base-passage-wiki-sec-mean-sentwise/bert-base-wikipedia-sections-mean-tokens-passage-part1.npy'
+TRAIN_EMB_VECS_DIR = '/media/sumanta/Seagate Backup Plus Drive/SentenceBERT_embeddings/sentbert_embeddings_by1train/bert-base-passage-wiki-sec-mean-sentwise'
 TEST_EMB_PIDS = '/media/sumanta/Seagate Backup Plus Drive/SentenceBERT_embeddings/sentbert_embeddings_by1test/bert-base-passage-wiki-sec-mean-sentwise/paraids_sents.npy'
-TEST_EMB_VECS = '/media/sumanta/Seagate Backup Plus Drive/SentenceBERT_embeddings/sentbert_embeddings_by1test/bert-base-passage-wiki-sec-mean-sentwise/bert-base-wikipedia-sections-mean-tokens-passage-part1.npy'
+TEST_EMB_VECS_DIR = '/media/sumanta/Seagate Backup Plus Drive/SentenceBERT_embeddings/sentbert_embeddings_by1test/bert-base-passage-wiki-sec-mean-sentwise'
+EMB_FILE_PREFIX = 'bert-base-wikipedia-sections-mean-tokens-passage'
+EMB_BATCH = 10000
 
-def make_psg_pair_embeddings(dat, emb_pid_file, emb_vec_file):
+class SentbertParaEmbedding():
+    def __init__(self, paraid_file, embed_dir, embed_file_prefix, batch_size):
+        self.para_info = None
+        self.paraids = list(np.load(paraid_file))
+        if('\t' in self.paraids[0]):
+            self.para_info = self.paraids
+            self.paraids = [p.split('\t')[0] for p in self.paraids]
+        self.emb_dir = embed_dir
+        self.prefix = embed_file_prefix
+        self.batch_size = batch_size
+        self.curr_para_emb = None
+        self.part = -1
+
+    def get_embeddings_as_dict(self, paraid_list):
+        print('Going to retrieve embeddings for ' + str(len(paraid_list)) + ' paras')
+        emb_dict = dict()
+        #part = -1
+        for p in paraid_list:
+            emb_dict[p] = self.get_single_embedding(p)
+        return emb_dict
+
+    def get_single_embedding(self, paraid):
+        #part = -1
+        if paraid in self.paraids:
+            p_index = self.paraids.index(paraid)
+        else:
+            print(paraid + ' not found in embedding dir')
+            return None
+        curr_part = p_index // self.batch_size + 1
+        offset = p_index % self.batch_size
+        if curr_part == self.part:
+            emb_vec = self.curr_para_emb[offset]
+        else:
+            self.curr_para_emb = np.load(self.emb_dir + '/' + self.prefix + '-part' + str(curr_part) + '.npy')
+            emb_vec = self.curr_para_emb[offset]
+            self.part = curr_part
+        return emb_vec
+
+    def get_single_sent_embedding(self, paraid):
+        if paraid in self.paraids:
+            p_index = self.paraids.index(paraid)
+            p_part = int(self.para_info[p_index].split('\t')[1])
+            p_offset = int(self.para_info[p_index].split('\t')[2])
+            p_len = int(self.para_info[p_index].split('\t')[3])
+        else:
+            print(paraid + ' not found in embedding dir')
+            return None
+        curr_part = p_part
+        if curr_part == self.part:
+            emb_vec = []
+            for i in range(p_len):
+                emb_vec.append(self.curr_para_emb[p_offset + i])
+        else:
+            self.curr_para_emb = np.load(self.emb_dir + '/' + self.prefix + '-part' + str(curr_part) + '.npy')
+            emb_vec = []
+            for i in range(p_len):
+                emb_vec.append(self.curr_para_emb[p_offset + i])
+            self.part = curr_part
+        return emb_vec
+
+    def get_sent_embeddings_as_dict(self, paraid_list):
+        emb_dict = dict()
+        for pid in paraid_list:
+            emb_dict[pid] = self.get_single_sent_embedding(pid)
+        return emb_dict
+
+# def make_psg_pair_embeddings(dat, emb_pid_file, emb_vec_file):
+#     emb_pid_dict = {}
+#     emb_pid_list = np.load(emb_pid_file)
+#     for l in emb_pid_list:
+#         emb_pid_dict[l.split('\t')[0]] = (int(l.split('\t')[1]), int(l.split('\t')[2]), int(l.split('\t')[3]))
+#     data_mat = []
+#     for t in dat:
+#         # we have to make the embeddings matrix on the fly ( 2nd parameter returned from this)
+#         p1 = t[1]
+#         p1dat = emb_pid_dict[p1]
+#         p1emb = list(range(p1dat[1], p1dat[1] + p1dat[2]))
+#         p2 = t[2].strip()
+#         p2dat = emb_pid_dict[p2]
+#         p2emb = list(range(p2dat[1], p2dat[1] + p2dat[2]))
+#         data_mat.append([t[0], p1emb, p2emb])
+#     data_mat = pd.DataFrame(data_mat, columns=['similar', 'p1', 'p2'])
+#     return data_mat, np.load(emb_vec_file)
+
+def make_psg_pair_embeddings(dat, emb_pid_file, emb_vec_dir, emb_file_prefix, batch_size):
     emb_pid_dict = {}
-    for l in np.load(emb_pid_file):
+    emb_pid_list = np.load(emb_pid_file)
+    for l in emb_pid_list:
         emb_pid_dict[l.split('\t')[0]] = (int(l.split('\t')[1]), int(l.split('\t')[2]), int(l.split('\t')[3]))
+    sent_embed = SentbertParaEmbedding(emb_pid_file, emb_vec_dir, emb_file_prefix, batch_size)
     data_mat = []
+    embeddings = []
+    emb_index = 0
+    print('Going to embed '+str(len(dat))+' parapair samples')
+    c = 0
     for t in dat:
-        p1dat = emb_pid_dict[t[1]]
-        p1emb = list(range(p1dat[1], p1dat[1] + p1dat[2]))
-        p2dat = emb_pid_dict[t[2].strip()]
-        p2emb = list(range(p2dat[1], p2dat[1] + p2dat[2]))
+        # we have to make the embeddings matrix on the fly ( 2nd parameter returned from this)
+        p1 = t[1]
+        # p1dat = emb_pid_dict[p1]
+        p1vec = sent_embed.get_single_sent_embedding(p1)
+        p1emb = list(range(emb_index, emb_index + len(p1vec)))
+        for v in p1vec:
+            embeddings.append(v)
+        emb_index += len(p1vec)
+
+        p2 = t[2].strip()
+        # p2dat = emb_pid_dict[p2]
+        p2vec = sent_embed.get_single_sent_embedding(p2)
+        p2emb = list(range(emb_index, emb_index + len(p2vec)))
+        for v in p2vec:
+            embeddings.append(v)
+        emb_index += len(p2vec)
         data_mat.append([t[0], p1emb, p2emb])
+        c += 1
+        if c % (len(dat) // 20) == 0:
+            print(str(c)+' samples embedded')
     data_mat = pd.DataFrame(data_mat, columns=['similar', 'p1', 'p2'])
-    return data_mat, np.load(emb_vec_file)
+    embeddings = np.array(embeddings)
+    return data_mat, embeddings
 
 def split_and_zero_padding(df, max_seq_length):
     # Split to dicts
@@ -75,7 +183,7 @@ class ManDist(Layer):
     def compute_output_shape(self, input_shape):
         return K.int_shape(self.result)
 
-def train(TRAIN_TSV, TEST_TSV, TRAIN_EMB_PIDS, TRAIN_EMB_VECS, TEST_EMB_PIDS, TEST_EMB_VECS, model_out_path, plot_path):
+def train(TRAIN_TSV, TEST_TSV, TRAIN_EMB_PIDS, TRAIN_EMB_DIR, TEST_EMB_PIDS, TEST_EMB_DIR, EMB_PREFIX, EMB_BATCH_SIZE, model_out_path, plot_path):
     # Load training set
     train_dat = []
     with open(TRAIN_TSV, 'r') as tr:
@@ -99,8 +207,8 @@ def train(TRAIN_TSV, TEST_TSV, TRAIN_EMB_PIDS, TRAIN_EMB_VECS, TEST_EMB_PIDS, TE
     max_seq_length = 20
     use_w2v = True
 
-    train_df, train_embeddings = make_psg_pair_embeddings(train_dat, TRAIN_EMB_PIDS, TRAIN_EMB_VECS)
-    test_df, test_embeddings = make_psg_pair_embeddings(test_dat, TEST_EMB_PIDS, TEST_EMB_VECS)
+    train_df, train_embeddings = make_psg_pair_embeddings(train_dat, TRAIN_EMB_PIDS, TRAIN_EMB_DIR, EMB_PREFIX, EMB_BATCH_SIZE)
+    #test_df, test_embeddings = make_psg_pair_embeddings(test_dat, TEST_EMB_PIDS, TEST_EMB_DIR, EMB_PREFIX, EMB_BATCH_SIZE)
 
     # Split to train validation
     validation_size = int(len(train_df) * 0.1)
@@ -198,8 +306,10 @@ def main():
     parser.add_argument('-te', '--test_dat', help='Path to test data in bert seq format', default=TEST_TSV)
     parser.add_argument('-ri', '--train_emb_pid', help='Path to train emb paraids', default=TRAIN_EMB_PIDS)
     parser.add_argument('-ti', '--test_emb_pid', help='Path to test emb paraids', default=TEST_EMB_PIDS)
-    parser.add_argument('-rv', '--train_emb_vec', help='Path to train emb vecs', default=TRAIN_EMB_VECS)
-    parser.add_argument('-tv', '--test_emb_vec', help='Path to test emb vecs', default=TEST_EMB_VECS)
+    parser.add_argument('-rv', '--train_emb_dir', help='Path to train emb dir', default=TRAIN_EMB_VECS_DIR)
+    parser.add_argument('-tv', '--test_emb_dir', help='Path to test emb dir', default=TEST_EMB_VECS_DIR)
+    parser.add_argument('-pre', '--emb_prefix', help='Embedding file prefix', default=EMB_FILE_PREFIX)
+    parser.add_argument('-bn', '--batch_size', help='Batch size of each embedding file shard', default=EMB_BATCH)
     parser.add_argument('-om', '--out_model', help='Path to save the model', default='./data/SiameseLSTM.h5')
     parser.add_argument('-op', '--out_plot', help='Path to save the history plot', default='./data/history-graph.png')
     args = vars(parser.parse_args())
@@ -207,11 +317,13 @@ def main():
     test_file = args['test_dat']
     train_emb_pid = args['train_emb_pid']
     test_emb_pid = args['test_emb_pid']
-    train_emb_vec = args['train_emb_vec']
-    test_emb_vec = args['test_emb_vec']
+    train_emb_vec_dir = args['train_emb_dir']
+    test_emb_vec_dir = args['test_emb_dir']
+    prefix = args['emb_prefix']
+    batch = args['batch_size']
     outmodel = args['out_model']
     outplot = args['out_plot']
-    train(train_file, test_file, train_emb_pid, train_emb_vec, test_emb_pid, test_emb_vec, outmodel, outplot)
+    train(train_file, test_file, train_emb_pid, train_emb_vec_dir, test_emb_pid, test_emb_vec_dir, prefix, batch, outmodel, outplot)
 
 if __name__ == '__main__':
     main()
